d501f09eaefad77d19ce0a1696cb674f471cb93c
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2017-06-15 15:13:22 +0700

    add files

---
+++ index.js
@@ -0,0 +1,11 @@
+// define toggles 
+var toggles = {foo: true, bar: false};
+ 
+// load them into the module 
+var featureToggles = require('feature-toggles');
+featureToggles.load(toggles);
+ 
+// check if a feature is enabled 
+if (featureToggles.isFeatureEnabled('foo')) {
+    console.log('foo');
+}
